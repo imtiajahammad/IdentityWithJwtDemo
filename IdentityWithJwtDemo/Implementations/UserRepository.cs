b4cd58ba245d7cfdc04ae3a60e455263eae9259c
Imtiaj Ahammad <imtiaj.pc@outlook.com>
2021-07-11 00:43:48 +0600

    UserRepository modify following IGenericRepository

--- IdentityWithJwtDemo/Implementations/UserRepository.cs
+++ IdentityWithJwtDemo/Implementations/UserRepository.cs
@@ -1,4 +1,8 @@
-﻿using IdentityWithJwtDemo.Interfaces;
+﻿using IdentityWithJwtDemo.Authentication;
+using IdentityWithJwtDemo.DomainRepositories;
+using IdentityWithJwtDemo.Interfaces;
+using IdentityWithJwtDemo.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,36 +10,122 @@ using System.Threading.Tasks;
 
 namespace IdentityWithJwtDemo.Implementations
 {
-    public class UserRepository : IUserRepository
+    public class UserRepository : GenericRepository<ApplicationUser>, IUserRepository
     {
-        public T Delete<T>(string id)
+        private readonly UserManager<ApplicationUser> _userManager;
+        public UserRepository(ApplicationDbContext context, UserManager<ApplicationUser> userManager) : base(context)
         {
-            throw new NotImplementedException();
+            this._userManager = userManager;
+        }
+        public async Task<StatusResult<string>> DeleteUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return new StatusResult<string> { Status = ResponseStatus.NotFound, Message = "user not found" };
+            }
+            var result = await _userManager.DeleteAsync(user);
+            if (result.Succeeded)
+            {
+                return new StatusResult<string> { Status = ResponseStatus.Success, Message = "User deleted" };
+            }
+            var errors = "";
+            foreach (var a in result.Errors)
+            {
+                errors += "|" + a.Description.ToString();
+            }
+            return new StatusResult<string> { Status = ResponseStatus.Failed, Message = errors.ToString(), Result = String.Empty };
+        }
+        public async Task<StatusResult<ApplicationUser>> GetUser(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return new StatusResult<ApplicationUser> { Status = ResponseStatus.NotFound, Message = "user not found" };
+            }
+            return new StatusResult<ApplicationUser> { Status = ResponseStatus.Success, Message = "User found", Result = user };
+        }
+        public async Task<StatusResult<ApplicationUser>> UpdateUser(EditUserViewModel editUserViewModel)
+        {
+            var user = await _userManager.FindByIdAsync(editUserViewModel.id);
+            if (user == null)
+            {
+                return new StatusResult<ApplicationUser> { Status = ResponseStatus.NotFound, Message = "user not found" };
+            }
+            user.UserName = editUserViewModel.name;
+            user.Email = editUserViewModel.email;
+            user.PhoneNumber = editUserViewModel.phoneNumber;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return new StatusResult<ApplicationUser> { Status = ResponseStatus.Success, Message = "User updated" };
+            }
+            return new StatusResult<ApplicationUser> { Status = ResponseStatus.Failed, Message = "something went wrong" };
+        }
+        public async Task<ApplicationUser> GetUserById(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return null;
+            }
+            return user;
+        }
+        public async Task<ApplicationUser> GetUserByUserName(string userName)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+            if (user == null)
+            {
+                return null;
+            }
+            return user;
         }
 
-        public T Delete<T>()
-        {
-            throw new NotImplementedException();
-        }
 
-        public T Get<T>()
+        #region roles
+        public async Task<IEnumerable<ApplicationUser>> GetUsersByRole(IdentityRole identityRole)
         {
-            throw new NotImplementedException();
+            return await _userManager.GetUsersInRoleAsync(identityRole.Name);
         }
-
-        public T Get<T>(T t)
+        public async Task<IdentityResult> GetUsersByRole(ApplicationUser applicationUser, IdentityRole identityRole)
         {
-            throw new NotImplementedException();
+            return await _userManager.AddToRoleAsync(applicationUser, identityRole.Name);
         }
-
-        public T Post<T>(T t)
+        public async Task<IdentityResult> RemoveUserFromRoleId(ApplicationUser applicationUser, string roleId)
         {
-            throw new NotImplementedException();
+            return await _userManager.RemoveFromRoleAsync(applicationUser, roleId);
         }
-
-        public T Put<T>(T t)
+        public async Task<IEnumerable<string>> GetRoleNamesByUser(ApplicationUser applicationUser)
         {
-            throw new NotImplementedException();
+            return await _userManager.GetRolesAsync(applicationUser);
+        }
+        #endregion roles
+
+
+        #region claims
+        public async Task<IEnumerable<System.Security.Claims.Claim>> GetClaimsByUser(ApplicationUser applicationUser)
+        {
+            return await _userManager.GetClaimsAsync(applicationUser);
+        }
+        public async Task<IdentityResult> AddClaimToUser(ApplicationUser applicationUser, System.Security.Claims.Claim claim)
+        {
+            return await _userManager.AddClaimAsync(applicationUser, claim);
+        }
+        public async Task<IdentityResult> RemoveClaimFromUser(ApplicationUser applicationUser, System.Security.Claims.Claim claim)
+        {
+            return await _userManager.RemoveClaimAsync(applicationUser, claim);
+        }
+        public async Task<IdentityResult> RemoveUserFromRoleId(ApplicationUser applicationUser, string oldPassword, string newPassword)
+        {
+            return await _userManager.ChangePasswordAsync(applicationUser, oldPassword, newPassword);
+        }
+        #endregion claims
+
+
+        public async Task<IdentityResult> ResetPasswordForUser(ApplicationUser user, string token, string newPassword)
+        {
+            var result = await _userManager.ResetPasswordAsync(user, token, newPassword);
+            return result;
         }
     }
 }
---
+++ IdentityWithJwtDemo/Interfaces/IUserRepository.cs
@@ -0,0 +1,43 @@
+﻿using IdentityWithJwtDemo.Authentication;
+using IdentityWithJwtDemo.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityWithJwtDemo.Interfaces
+{
+    public interface IUserRepository:IGenericRepository<ApplicationUser>
+    {
+        public Task<StatusResult<string>> DeleteUser(string id);
+        public Task<StatusResult<ApplicationUser>> GetUser(string id);
+        public Task<StatusResult<ApplicationUser>> UpdateUser(EditUserViewModel editUserViewModel);
+        public Task<ApplicationUser> GetUserById(string id);
+        public Task<ApplicationUser> GetUserByUserName(string userName);
+
+
+        #region roles
+        public Task<IEnumerable<ApplicationUser>> GetUsersByRole(IdentityRole identityRole);
+        public Task<IdentityResult> GetUsersByRole(ApplicationUser applicationUser, IdentityRole identityRole);
+        public Task<IdentityResult> RemoveUserFromRoleId(ApplicationUser applicationUser, string roleId);
+        public Task<IEnumerable<string>> GetRoleNamesByUser(ApplicationUser applicationUser);
+        #endregion roles
+
+
+        #region claims
+        public Task<IEnumerable<System.Security.Claims.Claim>> GetClaimsByUser(ApplicationUser applicationUser);
+
+        public Task<IdentityResult> AddClaimToUser(ApplicationUser applicationUser, System.Security.Claims.Claim claim);
+
+        public Task<IdentityResult> RemoveClaimFromUser(ApplicationUser applicationUser, System.Security.Claims.Claim claim);
+
+        public Task<IdentityResult> RemoveUserFromRoleId(ApplicationUser applicationUser, string oldPassword, string newPassword);
+
+        #endregion claims
+
+
+        public Task<IdentityResult> ResetPasswordForUser(ApplicationUser user, string token, string newPassword);
+        
+    }
+}
